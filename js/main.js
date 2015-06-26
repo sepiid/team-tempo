2d606b81979ba129779a771ba5ac0c6a003603b0
sepiid <sepiid@gmail.com>
2015-06-25 22:24:15 -0700

    -sepideh-june25-fixed product details

--- js/main.js
+++ js/main.js
@@ -7,175 +7,94 @@ $( document ).ready(function() {
   });
     var width= $( window ).width()/16;
     $('.close-detail').click(function() {
-        if(width>57){
-            $('.product-image').animate({
+        
+            $('.product-image').removeClass('active').animate({
             left:'-70em'
           },500 );
-          $('.product-details').animate({
+          $('.product-details').removeClass('active').animate({
             right:'-70em'
           },500 );
-        }  
-         else{
-          $('.product-image').animate({
-            top:'-70em'
-          },500 );
-          $('.product-details').animate({
-            bottom:'-70em'
-          },500 );
-        } 
+        
+         
     });
     $('#gravity').click(function() {
-    if(width>57){    
-        $('.gravity-image').animate({
+    
+        $('.gravity-image').addClass('active').animate({
           left:0
         },500 );
-        $('.gravity-detail').animate({
+        $('.gravity-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.gravity-image').animate({
-          top:0
-        },500 );
-        $('.gravity-detail').animate({
-          bottom:0
-        },500 );
-      
-      }
+
     });
     
     $('#polar').click(function() {
-      if(width>57){ 
-        $('.polar-image').animate({
+      
+        $('.polar-image').addClass('active').animate({
           left:0
         },500 );
-        $('.polar-detail').animate({
+        $('.polar-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.polar-image').animate({
-          top:0
-        },500 );
-        $('.polar-detail').animate({
-          bottom:0
-        },500 );
       
-      }
     });
     $('#iron').click(function() {
-      if(width>57){ 
-        $('.iron-image').animate({
+      $('.iron-image').addClass('active').animate({
           left:0
         },500 );
-        $('.iron-detail').animate({
+        $('.iron-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.iron-image').animate({
-          top:0
-        },500 );
-        $('.iron-detail').animate({
-          bottom:0
-        },500 );
-      
-      }
     });
     $('#sun').click(function() {
-      if(width>57){ 
-        $('.sun-image').animate({
+      
+        $('.sun-image').addClass('active').animate({
           left:0
         },500 );
-        $('.sun-detail').animate({
+        $('.sun-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.sun-image').animate({
-          top:0
-        },500 );
-        $('.sun-detail').animate({
-          bottom:0
-        },500 );
-      
-      }
+     
     });
     $('#trick').click(function() {
-      if(width>57){ 
-        $('.trick-image').animate({
+      
+        $('.trick-image').addClass('active').animate({
           left:0
         },500 );
-        $('.trick-detail').animate({
+        $('.trick-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.trick-image').animate({
-          top:0
-        },500 );
-        $('.trick-detail').animate({
-          bottom:0
-        },500 );
-      
-      }
+     
     });
     $('#sphere').click(function() {
-      if(width>57){ 
-        $('.sphere-image').animate({
+      
+        $('.sphere-image').addClass('active').animate({
           left:0
         },500 );
-        $('.sphere-detail').animate({
+        $('.sphere-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.sphere-image').animate({
-          top:0
-        },500 );
-        $('.sphere-detail').animate({
-          bottom:0
-        },500 );
-      
-      }
+     
     });
     $('#vinyl').click(function() {
-      if(width>57){ 
-        $('.vinyl-image').animate({
+      
+        $('.vinyl-image').addClass('active').animate({
           left:0
         },500 );
-        $('.vinyl-detail').animate({
+        $('.vinyl-detail').addClass('active').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.vinyl-image').animate({
-          top:0
-        },500 );
-        $('.vinyl-detail').animate({
-          bottom:0
-        },500 );
       
-      }
     });
     $('#solar').click(function() {
-      if(width>57){ 
+     
         $('.solar-image').animate({
           left:0
         },500 );
         $('.solar-detail').animate({
           right:0
         },500 );
-      }
-      else{
-        $('.solar-image').animate({
-          top:0
-        },500 );
-        $('.solar-detail').animate({
-          bottom:0
-        },500 );
-      
-      }
+    
     });
     
     
